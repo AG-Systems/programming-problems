d364b584427b8d432ff53e750994b10070114e98
Auriga <AG-Systems@users.noreply.github.com>
2016-12-21 17:54:39 -0800

    Update countduplicates.cpp

--- other/countduplicates.cpp
+++ other/countduplicates.cpp
@@ -1,25 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <algorithm> 
 
 std::vector <int> countinarray(std::vector <int> a)
 {
-    std::vector <int> counter = a;
+    std::sort(a.begin(), a.end());
+    std::vector <int> counter;
+    std::vector <int> numlist;
+    for(int i = 0; i < a.size(); i++)
+    {
+        numlist.push_back(i);
+        counter.push_back(0);
+    }
     for(int z = 0; z < a.size()-1; z++)
     {
         for(int x = (z+1); x < a.size(); x++)
         {
             if(a[z] == a[x])
             {
-                counter[z]++;  
-                std::cout << a[z] << std::endl;
+                if(a[z] != a[z-1])
+                {
+                    counter[(a[z])]++;
+                }
             }
         }
+    }
+    for(int z = 0; z < a.size(); z++)
+    {
+        if(counter[z] > 0)
+        {
+            counter[z]++;
+        }
+        std::cout << numlist[z] << ": " << counter[z] << std::endl;
     }
     return counter;
 }
 
 int main()
 {
-    std::vector <int> list = {1,1,2,3,4,5,6,7,7,8,0,1};
+    std::vector <int> list = {1,7,2,3,3,3,4,5,6,7,7,1,8,0,1,7,8,4,7};
     countinarray(list);
 }
