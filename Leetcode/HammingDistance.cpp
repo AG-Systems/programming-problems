595478e565cfbb8647cfe98fd724e26db0cbd61a
Auriga <AG-Systems@users.noreply.github.com>
2016-12-24 00:37:59 -0800

    Update HammingDistance.cpp

--- Leetcode/HammingDistance.cpp
+++ Leetcode/HammingDistance.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        x=x^y;
+        int res=0;
+        // get the total 1 digits
+        while(x>0){
+            if(x&1) res++; // if its final bit is 1, res++
+            x=x>>1;
+        }
+        return res;
+    }
+};
