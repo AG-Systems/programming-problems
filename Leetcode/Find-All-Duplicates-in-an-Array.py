ec6135b9bae2c433a95682acef8f97d722eeadde
Auriga <AG-Systems@users.noreply.github.com>
2017-01-30 22:26:53 -0800

    Create Find-All-Duplicates-in-an-Array.py

---
+++ Leetcode/Find-All-Duplicates-in-an-Array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        r = []
+        container = {}
+        for x in nums:
+            if x in container:
+                container[x] += 1
+                if container[x] > 1:
+                    r.append(x)
+            else:
+                container[x] = 1
+        return r
