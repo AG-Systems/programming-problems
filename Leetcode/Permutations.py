5e4c8d8d2c819d52255174c99de14bff489df9b9
Auriga <AG-Systems@users.noreply.github.com>
2017-07-01 14:02:36 -0700

    Create Permutations.py

---
+++ Leetcode/Permutations.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return list(itertools.permutations(nums))
