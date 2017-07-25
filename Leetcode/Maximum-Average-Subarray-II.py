17905b19b86d44c6a47d9c8121a3ccc2209031de
Auriga <AG-Systems@users.noreply.github.com>
2017-07-25 13:12:54 -0700

    Does not fully work

--- Leetcode/Maximum-Average-Subarray-II.py
+++ Leetcode/Maximum-Average-Subarray-II.py
@@ -5,12 +5,43 @@ class Solution(object):
         :type k: int
         :rtype: float
         """
+        import math
         container = []
-        nums.sort()
-        nums = nums[::-1]
-        counter = len(nums) - k
-        for x in range(counter):
-            summed = float(sum(nums[0:len(nums)-x]))
-            temp = summed / len(nums)
-            container.append(temp)
-        print container
+        sort_list = list(nums)
+        sort_list.sort()
+        sort_list = sort_list[::-1]
+        for x in range(1, len(sort_list)):
+            num1 = nums.index(sort_list[x-1])
+            num2 = nums.index(sort_list[x])
+            temp = nums[num1:num2+1]
+            if num2 < num1:
+                temp = nums[num2:num1+1] 
+            print temp
+            if len(temp) >= k:
+                container.append(float(sum(temp)) / len(temp))
+            else:
+                if 0 in nums:
+                    checker = False
+                    if abs(nums.index(0)-num1) == 1:
+                        if num1 > num2:
+                            num1 = num1 + 1
+                        else:
+                            num1 = num1 - 1
+                        checker = True
+                    elif abs(nums.index(0)-num2) == 1:
+                        if num1 > num2:
+                            num2 = num2 + 1
+                        else:
+                            num2 = num2 - 1
+                        checker = True
+                    temp = nums[num1:num2+1]
+                    if num2 < num1:
+                        temp = nums[num2:num1+1]
+                    if checker:
+                        container.append(float(sum(temp)) / len(temp)) 
+        if len(container) > 0:
+            return max(container)
+        else:
+            return float(nums[0])
+            
+            
