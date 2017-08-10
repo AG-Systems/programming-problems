588d27162c0beacf45385926d6f1bf88c102e9d9
Auriga <AG-Systems@users.noreply.github.com>
2017-08-10 13:55:11 -0700

    Create Nim-Game.py

---
+++ Leetcode/Nim-Game.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n % 4 == 0:
+            return False
+        else:
+            return True
+        
