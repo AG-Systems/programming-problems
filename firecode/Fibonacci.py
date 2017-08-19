ee185c194f042132b687c2b9499891c72cc00287
Auriga <AG-Systems@users.noreply.github.com>
2017-08-19 15:42:49 -0700

    Update Fibonacci.py

--- firecode/Fibonacci.py
+++ firecode/Fibonacci.py
@@ -1,8 +1,17 @@
-def fib(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
+def helper(n, table):
+    if n <= 1:
+        return n
     else:
-        return fib(n-1) + fib(n-2)
+        if table[n-1] == 0:
+            table[n-1] = helper(n-1,table)
+        if table[n-2] == 0:
+            table[n-2] = helper(n-2, table)
+        table[n] = table[n-1] + table[n-2]
+        return table[n]
+        
+def fib(n):
+    l = [0 for _ in range(n+1)]
+    results = helper(n, l)
+    return results
+    
     
