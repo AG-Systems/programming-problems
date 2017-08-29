ed926011fd77e96a666355375da6ec616d9d3b56
Auriga <AG-Systems@users.noreply.github.com>
2017-08-28 19:21:33 -0700

    Update Bubble-Sort.py

--- firecode/Bubble-Sort.py
+++ firecode/Bubble-Sort.py
@@ -6,4 +6,18 @@ def bubble_sort(a_list):
                 a_list[x] = a_list[x+1]
                 a_list[x+1] = temp
     return a_list
-
+"""
+def bubble_sort(a_list):
+    checker = True
+    while checker:
+        checker = False
+        for x in xrange(len(a_list)-1):
+            if a_list[x] > a_list[x+1]:
+                temp = a_list[x]
+                a_list[x] = a_list[x+1]
+                a_list[x+1] = temp
+                checker = True
+    return a_list
+    
+   # More understable code I guess  
+"""
