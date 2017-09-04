830827a5a9e5be8d97e52c03a6c09ee9cec1e4be
Auriga <AG-Systems@users.noreply.github.com>
2017-09-04 14:36:11 -0700

    Update Flip-it.py

--- firecode/Flip-it.py
+++ firecode/Flip-it.py
@@ -2,3 +2,12 @@ def flip_vertical_axis(matrix):
     for x in range(len(matrix)):
         matrix[x] = matrix[x][::-1]
         
+"""
+def flip_vertical_axis(matrix):
+    for row in range(len(matrix)):
+        temp = matrix[row]
+        temp = temp[::-1]
+        matrix[row] = temp
+        
+  Repractice
+"""
