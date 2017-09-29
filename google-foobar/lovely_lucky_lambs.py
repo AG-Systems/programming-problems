51a22987804dba48ccf57ad23e389de3828c70f5
Auriga <AG-Systems@users.noreply.github.com>
2017-09-28 18:02:00 -0700

    Update lovely_lucky_lambs.py

--- google-foobar/lovely_lucky_lambs.py
+++ google-foobar/lovely_lucky_lambs.py
@@ -1,46 +1,34 @@
-def fib_to(n):
-     fibs = [0, 1]
+def fib_to(n, total_lambs):
+     fibs = [1, 1]
+     temp = 2
      for i in range(2, n+1):
-         fibs.append(fibs[-1] + fibs[-2])
-     #fibs.remove(0)
-     return sum(fibs)
+         val = fibs[-1] + fibs[-2]
+         fibs.append(val)
+         temp += val
+         if temp > total_lambs:
+             break
+     if 0 in fibs:
+        fibs.remove(0)
+     if sum(fibs) > total_lambs:
+         del fibs[-1]
+     print(fibs)
+     return len(fibs)
+    
 def answer(total_lambs):
-    print("LAMBS: " + str(total_lambs))
-    import math
+    total = 0
     counter = 0
-    if total_lambs == 0:
-        return 0
-    while counter <= 1000000000:
-        if fib_to(counter) > total_lambs:
+    if total_lambs > 10**9 or total_lambs < 10:
+        gen = [1]
+    else:
+        gen = []
+    while counter <= total_lambs:
+        current_value = 2 ** counter
+        total += current_value
+        if total > total_lambs:
             break
-        else:
-            res = counter
+        gen.append(current_value)
         counter += 1
-    s = res
-    #g = (math.log(1 + total_lambs)) / math.log(2)
-    #g = int(g)
-    gen = [1,2,4]
-    total_lambs = total_lambs - sum(gen)
-    while total_lambs > 0:
-        n1 = gen[-1]
-        n2 = gen[-2]
-        n3 = gen[-3]
-        mul = n1 * 2
-        if mul > total_lambs:
-            summ = sum(gen)
-            x = total_lambs - summ
-            if x > (n1 + n2):
-                gen.append(x)
-                total_lambs = total_lambs - (x)
-            else:
-                break
-        else:
-            print(total_lambs, mul)
-            gen.append(mul)
-            total_lambs = total_lambs - mul
-    g = len(gen)
     print(gen)
-    print(s, g)
-    return abs(s - g)
+    return fib_to(len(gen)* 3, total_lambs) - len(gen)
 
 print(answer(10))
