1faf37459856553250bb29cff92538d9f1bb0861
Auriga <AG-Systems@users.noreply.github.com>
2017-10-02 21:52:00 -0700

    Update hey_i_already_did_that.py
    Test 1 passed!
    Test 2 failed.
    Test 3 failed.
    Test 4 passed!
    Test 5 passed!
    Test 6 passed!
    Test 7 failed.
    Test 8 failed.
    Test 9 passed!
    Test 10 failed.

--- google-foobar/hey_i_already_did_that.py
+++ google-foobar/hey_i_already_did_that.py
@@ -1,40 +1,48 @@
-def answer(n, b, link=[]):
-    # your code here
-    if b == 0:
-        print(link)
-        if link[-1] == link[-2]:
-            return 1
-        else:
-            #last = link[link[::-1].index(min(link))]
-            last = link[-1]
-            print(last)
-            counter = 2
-            while counter < len(link):
-                if link[-counter] == last:
-                    return counter
-                counter += 1
-            return len(link)
+def answer(n,b):
+    #n string
+    #b int
+    container = []
     k = len(n)
-    new_n = [int(i) for i in n]
-    x = list(new_n)
-    y = list(new_n)
-    x.sort(reverse=True)
-    y.sort()
-    x = int(''.join(str(e) for e in x))
-    y = int(''.join(str(e) for e in y))
-    #print(x,y)
-    z = int(x) - int(y)
-    if len(str(z)) == k:
-        n = z
-    else:
-        z = list(str(z))
-        z.insert(0, "0")
-        z = ''.join(str(e) for e in z)
-        n = z
-    link.append(z)
-    b = b - 1
-    return answer(str(n),b, link)
+    counter = 0
+    #n = str(int(n,b))
+    while counter <= 100:
+        new_n = [int(i) for i in n]
+        x = list(new_n)
+        y = list(new_n)
+        x.sort(reverse=True)
+        y.sort()
+        if len(x) < k:
+            while len(x) < k:
+                x.append(0)
+        if len(y) < k:
+            y.insert(0,"0")
+            while len(y) < k:
+                y.insert(0,0)
+
+        x = x[0:k]
+        x = ''.join(str(e) for e in x)
+        y = y[0:k]
+        y = ''.join(str(e) for e in y)
+        print(x,y)
+        z = int(x) - int(y)
+        if len(str(z)) < k:
+            lk = list(str(z))
+            while len(lk) < k:
+                lk.insert(0,"0")
+            print(lk)
+            lk = ''.join(str(e) for e in lk)
+        #z = int(str(z),b)
+        print(z)
+        if not str(z) in container:
+            container.append(str(z))
+        else:
+            last_num = container.index(str(z))
+            print(container, last_num)
+            return len(container) - last_num
+        n = str(z)
+        counter = counter + 1
+    print(container)
+    print()
+    print(set(container))
     
-
-print(answer("502212", 10))
-
+print(answer("1211", 10))
