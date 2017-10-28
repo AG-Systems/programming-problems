ecfb28c8e34be571c26c3ca206842f005596c4c2
Auriga <AG-Systems@users.noreply.github.com>
2017-10-27 22:37:03 -0700

    Update Moving-Zeros-To-The-End.py

--- Codewars/Moving-Zeros-To-The-End.py
+++ Codewars/Moving-Zeros-To-The-End.py
@@ -1,11 +1,23 @@
 def move_zeros(array):
     #your code here
-    num_list = []
-    z_list = []
-    for x in range(len(array)):
-        if array[x] != 0:
-            num_list.append(array[x])
-        else:
-            z_list.append(0)
-    array = num_list + z_list
-    return array
+    if array.count(False) > 0:
+        num_list = []
+        z_list = []
+        
+        for x in range(len(array)):
+            if array[x] == 0 or array[x] == 0.0:
+                if type(array[x]) == int or type(array[x]) == float:
+                    z_list.append(0)
+                else:
+                    num_list.append(array[x])
+            else:
+                num_list.append(array[x])
+
+        array = num_list + z_list
+        return array
+    else:
+        counter = array.count(0)
+        array = filter(lambda a: a != 0, array)
+        lk = [0] * counter
+        array = array + lk
+        return array
