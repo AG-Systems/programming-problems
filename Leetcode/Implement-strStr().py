fb5ddb577796d5c4ce5a5a2d9d413b094c9e57f7
Auriga <AG-Systems@users.noreply.github.com>
2017-10-28 14:13:53 -0700

    Update Implement-strStr().py

--- Leetcode/Implement-strStr().py
+++ Leetcode/Implement-strStr().py
@@ -5,7 +5,37 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        if not needle in haystack:
+        if needle == "":
+            return 0
+        elif len(needle) > len(haystack):
             return -1
-        else:
-            return haystack.index(needle)
+        elif needle not in haystack:
+            return -1
+        else:           
+            counter = 0
+            current_char = needle[counter]
+            index = -1
+            x = 0
+            while x < len(haystack):
+                print(current_char,haystack[x], index, x)
+                if current_char == haystack[x]:
+                    counter += 1
+                    if counter == 1:
+                        index = x
+                    if counter >= len(needle):
+                        return index
+                    current_char = needle[counter]
+                else:
+                    x = x - counter
+                    counter = 0
+                    current_char = needle[counter]
+                    index = -1
+                x = x + 1
+            return index
+    """
+    if not needle in haystack:
+        return -1
+    else:
+        return haystack.index(needle)
+    """
+                
