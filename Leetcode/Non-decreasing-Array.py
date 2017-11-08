c11070f2a9aeb2fde3870c7e569553977338dcae
Auriga <AG-Systems@users.noreply.github.com>
2017-11-07 17:39:41 -0800

    Update Non-decreasing-Array.py

--- Leetcode/Non-decreasing-Array.py
+++ Leetcode/Non-decreasing-Array.py
@@ -3,7 +3,7 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: bool
-        """
+
         counter = 0
         for x in range(0,len(nums)-1):
             if nums[x] > nums[x+1]:
@@ -13,5 +13,20 @@ class Solution(object):
                 return False
             else:
                 return True
+        else:
+            return False
+        """
+        list1 = list(nums)
+        list2 = list(nums)
+        for x in range(0,len(nums)-1):
+            if nums[x] > nums[x+1]:
+                list1[x] = nums[x + 1]
+                list2[x+1] = nums[x]
+                break
+        #print(list1,list2)
+        if list1 == sorted(list1):
+            return True
+        elif list2 == sorted(list2):
+            return True
         else:
             return False
