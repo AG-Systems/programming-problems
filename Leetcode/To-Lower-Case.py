bcab885e4e58b3e37bb7a6a16292b90ddcab11e7
Auriga <AG-Systems@users.noreply.github.com>
2018-09-04 21:42:32 -0700

    Update To-Lower-Case.py

--- Leetcode/To-Lower-Case.py
+++ Leetcode/To-Lower-Case.py
@@ -4,4 +4,45 @@ class Solution(object):
         :type str: str
         :rtype: str
         """
-        return str.lower()
+        
+        #return str.lower()
+        
+        #abcdefghijklmnopqrstuvwxyz
+        alphabet = {
+            "A": "a",
+            "B": "b",
+            "C": "c",
+            "D": "d",
+            "E": "e",
+            "F": "f",
+            "G": "g",
+            "H": 'h',
+            "I": "i",
+            "J": "j",
+            "K": "k",
+            "L": 'l',
+            "M": "m",
+            "N": "n",
+            "O": "o",
+            "P": 'p',
+            "Q": "q",
+            "R": "r",
+            "S": "s",
+            "T": 't',
+            "U": "u",
+            "V": "v",
+            "W": "w",
+            "X": 'x',
+            "Y": "y",
+            "Z": "z"
+        }
+        
+        lowercasestr = []
+        
+        for letter in str:
+            if letter in alphabet:
+                lowercasestr.append(alphabet[letter])
+            else:
+                lowercasestr.append(letter)
+        return ''.join(lowercasestr)
+        
