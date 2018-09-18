03a627becd89a65747a9b09a2ed0e4bcffcb03d0
Auriga <AG-Systems@users.noreply.github.com>
2018-09-18 11:22:28 -0700

    Update NUM1BITS.py

--- codelab-fb/NUM1BITS.py
+++ codelab-fb/NUM1BITS.py
@@ -7,3 +7,19 @@ class Solution:
             return int(binary.count('1'))
         else:
             return 0 #error
+
+      
+ # Other solution: 
+"""
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def numSetBits(self, A):
+        ret = 0
+        while A != 0:
+            if A&1:
+                ret += 1
+            A = A >> 1
+        return ret
+
+"""
