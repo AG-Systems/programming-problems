63e78a30a2fb50082ac79a64ec5df87401687589
Auriga <AG-Systems@users.noreply.github.com>
2018-09-22 16:49:26 -0700

    Update Leaf-Similar-Trees.py

--- Leetcode/Leaf-Similar-Trees.py
+++ Leetcode/Leaf-Similar-Trees.py
@@ -29,3 +29,33 @@ class Solution(object):
             return True
         else:
             return False
+
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def helper(self, root, container):
+        if root == None:
+            return container
+        if root.left == None and root.right == None:
+            container.append(root.val)
+            return container
+        self.helper(root.left, container)
+        self.helper(root.right, container)
+        return container
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: bool
+        """
+        seq1 = self.helper(root1, [])
+        seq2 = self.helper(root2, [])
+        
+        return seq1 == seq2
+'''
