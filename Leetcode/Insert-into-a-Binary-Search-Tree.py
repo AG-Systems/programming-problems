6a8a5a1efcc3b4c81729fc876757cf0727a1cb31
Auriga <AG-Systems@users.noreply.github.com>
2018-09-23 16:49:24 -0700

    Create Insert-into-a-Binary-Search-Tree.py

---
+++ Leetcode/Insert-into-a-Binary-Search-Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root == None:
+            return TreeNode(val)
+        
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
