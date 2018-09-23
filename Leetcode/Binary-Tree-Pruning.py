313137121bb975a90c54c9a43867eb5ba5c1d779
Auriga <AG-Systems@users.noreply.github.com>
2018-09-23 11:34:44 -0700

    Create Binary-Tree-Pruning.py

---
+++ Leetcode/Binary-Tree-Pruning.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def pruneTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None:
+            return None
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        
+        if root.val == 1:
+            return root
+        if root.val == 0 and (root.left == None and root.right == None):
+            return None
+        return root
+        
