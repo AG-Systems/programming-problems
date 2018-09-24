5c26b35cec97e52977130be85db2ded29141af26
Auriga <AG-Systems@users.noreply.github.com>
2018-09-23 21:35:47 -0700

    Create Design-Linked-List.py

---
+++ Leetcode/Design-Linked-List.py
@@ -0,0 +1,76 @@
+class MyLinkedList(object):
+    class Node(object):
+        def __init__(self, val):
+            self.val = val
+            self.next = None
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = self.Node()
+
+    def get(self, index):
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        :type index: int
+        :rtype: int
+        """
+        head = self.root
+        counter = 0
+        while head:
+            if counter == index:
+                return head.val
+            head = head.next
+            counter += 1
+        return -1
+        
+
+    def addAtHead(self, val):
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        head = self.root
+        newHead = self.Node(val)
+        newHead.next = head
+        head = newHead
+        return 
+
+    def addAtTail(self, val):
+        """
+        Append a node of value val to the last element of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        head = self.root
+        while head:
+            head = head.next
+        head = val
+        return
+
+    def addAtIndex(self, index, val):
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        :type index: int
+        :type val: int
+        :rtype: void
+        """
+        
+
+    def deleteAtIndex(self, index):
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        :type index: int
+        :rtype: void
+        """
+        
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
