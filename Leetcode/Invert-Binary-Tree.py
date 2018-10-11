1339602bdc45821940d778e3a438e725ffd3cd62
Auriga <AG-Systems@users.noreply.github.com>
2018-10-10 18:42:57 -0700

    Update Invert-Binary-Tree.py

--- Leetcode/Invert-Binary-Tree.py
+++ Leetcode/Invert-Binary-Tree.py
@@ -42,4 +42,23 @@ class Solution(object):
                 return root
             else:
                 return root
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root:
+            temp = root.left
+            root.left = self.invertTree(root.right)
+            root.right = self.invertTree(temp)
+            return root
 '''
