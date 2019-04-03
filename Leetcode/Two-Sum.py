2bf14eb858f338097931818df172e1b289b40069
Auriga <AG-Systems@users.noreply.github.com>
2019-04-03 11:25:52 -0700

    Update Two-Sum.py

--- Leetcode/Two-Sum.py
+++ Leetcode/Two-Sum.py
@@ -22,3 +22,36 @@ class Solution(object):
                     indices.append(nums.index(val))
                 break
         return indices
+
+    
+"""
+# BETTER SOLUTION
+class Solution(object):
+    def twoSum(self, nums, target):        
+        container = {}
+        
+        counter = 0
+        
+        for num in nums:
+            if num in container:
+                container[num]["index"].append(counter)
+            else:
+                diff = target - num
+                container[num] = { 
+                    "diff": diff,
+                    "index": [counter]
+                }
+            counter += 1
+        
+        for key,val in container.items():
+            data = val
+            if data["diff"] in container and data["diff"] + key == target:
+                if data["diff"] != key:
+                    index1 = data["index"][0]
+                    index2 = container[data["diff"]]["index"][0]
+                    return [index1, index2]
+                else:
+                    index1 = data["index"][0]
+                    index2 = data["index"][1]
+                    return [index1, index2]
+"""
