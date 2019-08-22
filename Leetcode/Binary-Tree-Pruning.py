3795b2e2a5a31dd6d03fb6679e9ff9df3dc504f1
Auriga <AG-Systems@users.noreply.github.com>
2019-08-21 17:55:03 -0700

    Update Binary-Tree-Pruning.py

--- Leetcode/Binary-Tree-Pruning.py
+++ Leetcode/Binary-Tree-Pruning.py
@@ -22,3 +22,26 @@ class Solution(object):
             return None
         return root
         
+
+  """
+  # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        if root == None:
+            return None
+    
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        
+        if root.val == 0 and root.left == None and root.right == None:
+            root = None
+        return root
+        
+        
+ """     
