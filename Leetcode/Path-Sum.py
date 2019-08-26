ad7bd639376032a34c624b001323c5bd0d4bbbda
Auriga <AG-Systems@users.noreply.github.com>
2019-08-25 19:49:11 -0700

    Update Path-Sum.py

--- Leetcode/Path-Sum.py
+++ Leetcode/Path-Sum.py
@@ -23,3 +23,30 @@ class Solution(object):
                 
             
             
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        self.has_sum = False
+        def dfs(root, sum_path, goal_sum):
+            if root:
+                sum_path += root.val
+                if root.left == None and root.right == None:
+                    if sum_path == goal_sum:
+                        self.has_sum = True
+                        return True                    
+                dfs(root.left, sum_path, goal_sum)
+                dfs(root.right, sum_path, goal_sum)
+
+
+        
+        dfs(root, 0, sum)
+        
+        return self.has_sum
+"""
