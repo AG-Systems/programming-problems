9727e5530ed3ea55976f28608e71449f6a834793
Auriga <AG-Systems@users.noreply.github.com>
2019-08-28 16:13:37 -0700

    Update Odd-Even-Linked-List.py

--- Leetcode/Odd-Even-Linked-List.py
+++ Leetcode/Odd-Even-Linked-List.py
@@ -23,3 +23,32 @@ class Solution(object):
         container.extend(r)
         return container
             
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def oddEvenList(self, head):
+        if head == None:
+            return None
+        odd = head
+        even = head.next
+        even_head = even
+        
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = even_head
+        return head
+        
+        
+        
+            
+        
+        
+   """
