f2c90c2d5f0eb2ceed3385a46638320e157f61b2
Auriga <AG-Systems@users.noreply.github.com>
2019-08-28 17:28:37 -0700

    Update Palindrome-Linked-List.py

--- Leetcode/Palindrome-Linked-List.py
+++ Leetcode/Palindrome-Linked-List.py
@@ -10,6 +10,55 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
+        if head == None:
+            return True
+        
+        current = head
+        length = 0
+        while current:
+            current = current.next
+            length += 1
+        
+        current = head
+        is_even = length % 2 == 0
+        if is_even:
+            length = length / 2
+        else:
+            import math
+            length = math.ceil(length / 2)
+        
+        seeker = head
+        reverse_nodes = None
+        future = None
+        
+        counter = 0
+        while seeker and length > 0:
+            future = seeker.next
+            seeker.next = reverse_nodes
+            reverse_nodes = seeker
+            seeker = future
+            length -= 1
+        if not is_even:
+            seeker = seeker.next
+
+        while reverse_nodes and seeker:
+            if reverse_nodes.val != seeker.val:
+                return False
+            reverse_nodes = reverse_nodes.next
+            seeker = seeker.next
+        return True
+            
+            
+            
+"""        
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def isPalindrome(self, head):
         node = head
         length = 0
         container = []
@@ -31,4 +80,4 @@ class Solution(object):
         else:
             return True
         
-        
+ """       
