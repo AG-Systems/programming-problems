72c567e5f9d8dfd62d58791e1acb40bad14e7d78
Auriga <AG-Systems@users.noreply.github.com>
2019-08-31 01:27:13 -0700

    Update Plus-One.py

--- Leetcode/Plus-One.py
+++ Leetcode/Plus-One.py
@@ -18,3 +18,18 @@ class Solution(object):
         return answer
         
                 
+"""
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits[len(digits) - 1] += 1
+
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] > 9:
+                digits[i] %= 10
+                if (i - 1 < 0):
+                    digits.insert(0, 1)
+                else:
+                    digits[i - 1] += 1
+
+        return digits
+"""
