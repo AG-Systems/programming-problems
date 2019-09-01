297132e748ce606dad3b590e05ed2f93a7c21cca
Auriga <AG-Systems@users.noreply.github.com>
2019-08-31 18:54:38 -0700

    Update Move-Zeroes.py

--- Leetcode/Move-Zeroes.py
+++ Leetcode/Move-Zeroes.py
@@ -13,6 +13,24 @@ class Solution(object):
                     nums[nonzero] = nums[counter]
                     nums[counter] = 0
             counter += 1
+
+"""
+class Solution(object):
+    def moveZeroes(self, nums):
+        if len(nums) == 0:
+            return
+        
+        index = 0
+        for digit in nums:
+            if digit != 0:
+                nums[index] = digit
+                index += 1
+        
+        
+        for zero in range(index, len(nums)):
+            nums[zero] = 0
+        return
+"""
                 
                 
             
