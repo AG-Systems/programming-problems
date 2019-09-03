eebfd2fd480f870c63c6543b0e798448139fa543
Auriga <AG-Systems@users.noreply.github.com>
2019-09-02 21:30:23 -0700

    Update Unique-Paths.py

--- Leetcode/Unique-Paths.py
+++ Leetcode/Unique-Paths.py
@@ -5,14 +5,17 @@ class Solution:
             row = [0] * m
             grid.append(row)
         
-        def unique_paths(grid, row, col):
-            if row > len(grid[0]) - 1 or col > len(grid) - 1:
+        def unique_paths(grid, row, col, paths):
+            if col > len(grid[0]) - 1 or row > len(grid) - 1:
                 return 0
-            if row == len(grid[0]) - 1 and col == len(grid) - 1:
+            if col == len(grid[0]) - 1 and row == len(grid) - 1:
                 return 1
-            return unique_paths(grid, row + 1, col) + unique_paths(grid, row, col + 1)
+            if paths[row][col] == 0:
+                paths[row][col] = unique_paths(grid, row + 1, col, paths) + unique_paths(grid, row, col + 1, paths)
+            return paths[row][col]
         
         if grid != []:
-            return unique_paths(grid, 0, 0)
+            paths = list(grid)
+            return unique_paths(grid, 0, 0, paths)
         else:
             return 0
