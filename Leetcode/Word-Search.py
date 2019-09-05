44920cb2a36c1700c7dcd697ec87aa5873b37e91
Auriga <AG-Systems@users.noreply.github.com>
2019-09-04 17:24:34 -0700

    Update Word-Search.py

--- Leetcode/Word-Search.py
+++ Leetcode/Word-Search.py
@@ -6,11 +6,14 @@ class Solution:
             else:
                 return False
             
+        self.visited = set()
+        
         def is_valid_space(grid, row, col):
-            if row == len(grid) or row < 0 or col == len(grid[0]) or col < 0:
+            if row >= len(grid) or row < 0 or col >= len(grid[0]) or col < 0 or (row, col) in self.visited:
                 return False
             else:
                 return True
+            
         def search(grid, row, col, word, index):
             if not is_valid_space(grid, row, col):
                 return False
@@ -19,15 +22,17 @@ class Solution:
             if word[index] != grid[row][col]:
                 return False
             
-            temp = grid[row][col]
-            grid[row][col] = " "
+            self.visited.add((row, col))
+            
             left = search(grid, row, col - 1, word, index + 1)
             right = search(grid, row, col + 1, word, index + 1)
             down = search(grid, row + 1, col, word, index + 1)
             up = search(grid, row - 1, col, word, index + 1)
-            
-            grid[row][col] = temp
-            return left or right or down or up
+            found = left or right or down or up
+            if not found:
+                self.visited.remove((row, col))
+            return found
+        
         
         for row in range(0, len(board)):
             for col in range(0, len(board[0])):
