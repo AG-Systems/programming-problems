86d25a65390ade80ce8848ec086f95af3994c8cb
Auriga <AG-Systems@users.noreply.github.com>
2019-09-05 18:26:27 -0700

    Update Trim-a-Binary-Search-Tree.py

--- Leetcode/Trim-a-Binary-Search-Tree.py
+++ Leetcode/Trim-a-Binary-Search-Tree.py
@@ -24,3 +24,31 @@ class Solution(object):
         if root.val > R:
             return root.left
         return root
+
+    """
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def trimBST(self, root: TreeNode, L: int, R: int) -> TreeNode:
+        if root == None:
+            return None
+        def trim(node):
+            if not node:
+                return None
+            elif node.val > R:
+                return trim(node.left)
+            elif node.val < L:
+                return trim(node.right)
+            else:
+                node.left = trim(node.left)
+                node.right = trim(node.right)
+                return node
+        
+        return trim(root)
+                
+"""
