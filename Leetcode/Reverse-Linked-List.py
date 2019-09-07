f9870e130ad9022e959be3cabc0568a609679463
Auriga <AG-Systems@users.noreply.github.com>
2019-09-07 16:55:06 -0700

    Update Reverse-Linked-List.py

--- Leetcode/Reverse-Linked-List.py
+++ Leetcode/Reverse-Linked-List.py
@@ -24,3 +24,20 @@ class Solution(object):
             counter += 1
         return head
             
+"""
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head == None or head.next == None:
+            return head
+        
+        def helper(current, prev = None):
+            if current == None:
+                return prev
+            
+            future = current.next
+            current.next = prev
+            prev = current
+            current = future
+            return helper(current, prev)
+        
+        return helper(head)
+"""
