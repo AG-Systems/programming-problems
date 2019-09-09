6777e914d7159c6f4bc4b8247d985ac818b29f17
Auriga <AG-Systems@users.noreply.github.com>
2019-09-08 21:43:23 -0700

    Update Validate-Binary-Search-Tree.py

--- Leetcode/Validate-Binary-Search-Tree.py
+++ Leetcode/Validate-Binary-Search-Tree.py
@@ -19,3 +19,19 @@ class Solution(object):
                 return check(node.left, left_limit, node.val) and check(node.right, node.val, right_limit)
             return False
         return check(root, -1*float("inf"), float("inf"))
+    
+"""
+        if root == None:
+            return True
+        
+        def is_bst_helper(root, left_val, right_val):
+            if root == None:
+                return True
+            
+            if root.val <= left_val or root.val >= right_val:
+                return False
+            
+            return is_bst_helper(root.left, left_val, root.val) and is_bst_helper(root.right, root.val, right_val)
+        
+        return is_bst_helper(root, float("-inf"), float("inf"))
+"""
