6bac0673a17e20805fc4d471eef83cd31ba82feb
Auriga <AG-Systems@users.noreply.github.com>
2019-09-08 23:30:36 -0700

    Update Missing-Number.py

--- Leetcode/Missing-Number.py
+++ Leetcode/Missing-Number.py
@@ -14,3 +14,24 @@ class Solution(object):
                 return m-1
         if m == -1:
             return len(nums)
+"""
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        total_seq_xor = 0
+        current_num_xor = 0
+        
+        x = 0
+        while x <= len(nums):
+            total_seq_xor ^= x
+            x += 1
+            
+        for x in range(0, len(nums)):
+            num = nums[x]
+            current_num_xor ^= num
+        
+        return total_seq_xor ^ current_num_xor
+"""
+            
+        
+            
+        
