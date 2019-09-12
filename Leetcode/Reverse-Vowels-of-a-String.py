25d4d22995d92c786f8d57930aabc16d42d4d1f0
Auriga <AG-Systems@users.noreply.github.com>
2019-09-11 19:43:33 -0700

    Update Reverse-Vowels-of-a-String.py

--- Leetcode/Reverse-Vowels-of-a-String.py
+++ Leetcode/Reverse-Vowels-of-a-String.py
@@ -17,3 +17,32 @@ class Solution(object):
         newstring = ''.join(map(str, container))
         return newstring
         
+ """
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        if len(s) == 0:
+            return ""
+        s = list(s)
+        vowel_list = {"a", "e", "o", "u", "i", "A", "E", "O", "U", "I"}
+        
+        left_ptr = 0
+        right_ptr = len(s) - 1
+        
+        while left_ptr < right_ptr:
+            if s[left_ptr] in vowel_list and s[right_ptr] in vowel_list:
+                temp = s[left_ptr]
+                s[left_ptr] = s[right_ptr]
+                s[right_ptr] = temp
+                right_ptr -= 1
+                left_ptr += 1
+            else:
+                if s[left_ptr] in vowel_list:
+                    right_ptr -= 1
+                elif s[right_ptr] in vowel_list:
+                    left_ptr += 1
+                else:
+                    left_ptr += 1
+                    right_ptr -= 1
+        
+        return "".join(s)
+"""
