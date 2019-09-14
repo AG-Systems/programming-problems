6205d62cea11dba1a5689db7e77668eb4efa7cee
Auriga <AG-Systems@users.noreply.github.com>
2019-09-14 00:56:24 -0700

    Update Find-the-Duplicate-Number.py

--- Leetcode/Find-the-Duplicate-Number.py
+++ Leetcode/Find-the-Duplicate-Number.py
@@ -12,3 +12,24 @@ class Solution(object):
                     return x
             else:
                 container[x] = 1
+
+"""
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[0]
+        slow = nums[slow]
+        fast = nums[nums[fast]]        
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+                
+        ptr_1 = nums[0]
+        ptr_2 = slow
+        while ptr_1 != ptr_2:
+            ptr_1 = nums[ptr_1]
+            ptr_2 = nums[ptr_2]
+        return ptr_1
+"""
