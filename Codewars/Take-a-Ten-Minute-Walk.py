0db98ea2b2a221dde0484d1baf9be56be3ace10b
Auriga <AG-Systems@users.noreply.github.com>
2019-09-19 03:39:08 -0700

    Create Take-a-Ten-Minute-Walk.py

---
+++ Codewars/Take-a-Ten-Minute-Walk.py
@@ -0,0 +1,22 @@
+def isValidWalk(walk):
+    if len(walk) == []:
+        return True
+    if len(walk) > 10:
+        return False
+    pos_table = {
+        "n": 0,
+        "s": 0,
+        "e": 0,
+        "w": 0
+    }
+    
+    for pos in walk:
+        pos_table[pos] += 1
+    
+    if pos_table["n"] == pos_table["s"]:
+        if pos_table["e"] == pos_table["w"]:
+            return True
+        else:
+            return False
+    else:
+        return False
