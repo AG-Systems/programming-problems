450ccf1d2fb7f1bc2683d245e889f5a52f69d727
auriga <AG-Systems@users.noreply.github.com>
2023-03-29 22:02:48 -0700

    Update Product-of-Array-Except-Self.py

--- Leetcode/Product-of-Array-Except-Self.py
+++ Leetcode/Product-of-Array-Except-Self.py
@@ -1,20 +1,19 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        output = []
-        counter = 0
-        while counter < len(nums):
-            temp = list(nums)
-            del temp[counter]
-            x = str(temp)
-            mul_nums = x.replace(',','*')
-            product = eval(mul_nums)
-            product = str(product).replace("[","")
-            product = str(product).replace("]","")
-            output.append(int(product))
-            counter += 1
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = [None] * len(nums)
+        postfix = [None] * len(nums)
+        output = [None] * len(nums)
+
+        prefix[0] = nums[0]
+        for current_index in range(1, len(nums)):
+            prefix[current_index] = prefix[current_index - 1] * nums[current_index]
+        postfix[-1] = nums[-1]
+        for current_index in reversed(range(0, len(nums) - 1)):
+            postfix[current_index] = postfix[current_index + 1] * nums[current_index]
+        # print(prefix, postfix)
+
+        output[0] = 1 * postfix[1]
+        output[-1] = 1 * prefix[-2]
+        for num in range(1, len(nums) - 1):
+            output[num] = prefix[num - 1] * postfix[num + 1]
         return output
-             
